e34951a8a3a71e300f6a647d666fa1257ac1fda2
vemvb123 <vemundbrynjulfsen@gmail.com>
2026-02-16 05:27:49 +0100

    Added older training file, to check by new models performs bad

--- code/training/check.py
+++ code/training/check.py
@@ -1,11 +1,8 @@
-"""
-diffusion.py contains code for training diffusion models
-"""
-from diffusers import CosineDPMSolverMultistepScheduler, DDPMScheduler
+
+
 
 import logging
 
-from code.training.experimental import run_epoch_feature
 from code.training.noising import get_diffusion_schedule, get_noised_x
 from code.training.utils import get_dataset_loaders, get_models, mask_invalid, plot_losses
 
@@ -56,10 +53,85 @@ Prosseseringstid:
 '''
 
 
-def run_epoch(loop, device, timesteps,
+def run_epoch_feature(loop, device, timesteps,
             model_adj, model_enc, optimizer,
-            batch_size, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod, mode, valid_h, valid_w, 
-            scheduler=None):
+            batch_size, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod, mode):
+
+    total_loss = 0.0
+    for batch_idx, (target_assignments, proc_times, job_id, pos_job) in enumerate(loop): 
+        """ 
+        logging.info("dataset shapes")
+        logging.info(proc_times.shape)
+        logging.info(job_id.shape)
+        logging.info(pos_job.shape)
+        """
+
+        features = torch.cat([
+            proc_times,
+            job_id,
+            pos_job,
+        ], dim=1)
+
+
+        # logging.info(f"f shape {features.shape}")
+        features = features.to(device, dtype=torch.float32)
+        target_assignments = target_assignments.to(device, dtype=torch.float32)
+
+        B = target_assignments.shape[0]
+        if B != batch_size:
+            logging.warning(f"Skipping batch {batch_idx} with size {B}")
+            continue
+ 
+
+        # Sample random timesteps
+        t = torch.randint(0, timesteps, (batch_size,), device=device) 
+        # logging.info(f't shape {t.shape}')
+
+        # encode images
+        optimizer.zero_grad()
+        enc_f = model_enc(features, t, type_t="timestep")
+        # logging.info(f"encoded f {enc_f.shape}")
+        
+        # Sample noise
+        noise, noised = get_noised_x(t, target_assignments, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod)
+        #logging.info("catination")
+        #logging.info(noise.shape)
+        #logging.info(enc_f.shape)
+
+        model_input = torch.cat([
+                noised,
+                enc_f,
+            ], dim=1)
+        
+        #logging.info(model_input.shape)
+
+        # Predict noise
+        pred = model_adj(model_input, t, type_t="timestep")
+        #logging.info(f"made prediction, {pred.shape}")
+
+        pred_valid, noise_valid = mask_invalid(4, 16, pred, noise)
+        loss = nn.MSELoss()(pred_valid,noise_valid)
+        # loss = nn.MSELoss()(pred, noise)
+        if mode == "train":
+            loss.backward()
+            optimizer.step()
+
+        # Save the loss value
+        loss_value = loss.item()
+        total_loss += loss_value
+
+        loop.set_postfix(loss=loss_value)
+
+    return loop, model_adj, model_enc, total_loss / len(loop)
+
+
+
+
+
+
+def run_epoch_adj(loop, device, timesteps,
+            model_adj, model_enc, optimizer,
+            batch_size, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod, mode, valid_h, valid_w):
 
 
     total_loss = 0.0
@@ -82,14 +154,7 @@ def run_epoch(loop, device, timesteps,
 
         # Sample random timesteps
         t = torch.randint(0, timesteps, (batch_size,), device=device)
-
-        noise, noised = None, None
-        if scheduler == None:
-            noise, noised = get_noised_x(t, target_assignments, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod)
-        else:
-            noise = torch.randn_like(target_assignments)
-            noised = scheduler.add_noise(target_assignments, noise, t)
-
+        noise, noised = get_noised_x(t, target_assignments, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod)
 
         model_input = torch.cat([
             noised,
@@ -116,6 +181,25 @@ def run_epoch(loop, device, timesteps,
     return loop, model_adj, total_loss / len(loop)
 
 
+def count_nan_indices(loader):
+    total_nans = 0
+
+    for batch in loader:
+        target_assignments, proc_times, job_ops_adj, ops_ma_adj = batch
+
+        B = target_assignments.shape[0]
+
+        for i in range(B):
+            if (
+                torch.isnan(target_assignments[i]).any() or
+                torch.isnan(proc_times[i]).any() or
+                torch.isnan(job_ops_adj[i]).any() or
+                torch.isnan(ops_ma_adj[i]).any()
+            ):
+                total_nans += 1
+    logging.info(f"Total instances with NaN values: {total_nans}")
+    return total_nans
+
 def diffusion(
     model_type: str, # must be either "adj" for adjecency model or "f" for feature vector model
     train_dataset,
@@ -126,43 +210,30 @@ def diffusion(
     graph_name: str,
     graph_save_folder: str,
     valid_h, valid_w,
-    timesteps: int = 1000,
     num_epochs: int = 100,
     lr: float = 1e-3,
     device: str = "cuda",
     batch_size: int = 32,
-    use_cos=True
 ):
+
     n_base_features = train_dataset.n_base_features
 
-    beta_start = 1e-4
-    beta_end = 0.02
-    sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod = get_diffusion_schedule(beta_start, beta_end, timesteps)
-
-
-    scheduler = None
-    if use_cos:
-        scheduler = DDPMScheduler(
-            num_train_timesteps=timesteps,
-            beta_start=beta_start,
-            beta_end=beta_end,
-            beta_schedule="squaredcos_cap_v2",  # cosine schedule
-            clip_sample=True,
-            prediction_type="epsilon",
-        )
-
-
     if model_type != "adj" and model_type != "f":
-        raise ValueError(f"model_type must be either adj or f .kk. but value was #{model_type}#")
+        raise ValueError(f"model_type must be either adj or f .. but value was #{model_type}#")
 
     # running different diffusion algorithms depening on the model type
     run_epoch_func = None
     if model_type == "adj":
-        run_epoch_func = run_epoch
+        run_epoch_func = run_epoch_adj
     elif model_type == "f":
         run_epoch_func = run_epoch_feature 
 
-    train_loader, test_loader = get_dataset_loaders(train_dataset, test_dataset, batch_size=batch_size) # subset=True ... for testing med subset
+    train_loader, test_loader = get_dataset_loaders(train_dataset, test_dataset, batch_size=batch_size)
+    #count_nan_indices(train_loader)
+    #count_nan_indices(test_loader)
+    #logging.info("exit")
+    #exit()
+
 
     logging.info(f"N instances in train dataset: { len(train_loader.dataset) }")
     logging.info(f"N instances in test dataset: { len(test_loader.dataset) }")
@@ -171,6 +242,11 @@ def diffusion(
 
     trainable_params = sum(p.numel() for p in model_adj.parameters() if p.requires_grad)
     logging.info(f"Amount of trainable parameters: {trainable_params}")
+
+    beta_start = 1e-4
+    beta_end = 0.02
+    timesteps = 1000
+    sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod = get_diffusion_schedule(beta_start, beta_end, timesteps)
 
     all_losses = []
     all_losses_test = []
@@ -192,14 +268,14 @@ def diffusion(
             train_loop, device, timesteps,
             model_adj, model_enc, optimizer,
             batch_size, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod,
-            "train", valid_h, valid_w, scheduler
+            mode="train", valid_h=valid_h, valid_w=valid_w
         )
 
         test_loop, model_adj, avg_loss_test = run_epoch_func(
             test_loop, device, timesteps,
             model_adj, model_enc, optimizer,
             batch_size, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod,
-            "test", valid_h, valid_w, scheduler
+            mode="test", valid_h=valid_h, valid_w=valid_w
         )
 
 
@@ -235,7 +311,7 @@ def diffusion(
     torch.save(model_adj.state_dict(), model_path_adj)
     if model_type == "f":
         torch.save(model_enc.state_dict(), model_enc)
-    print(f"done training. Saved model {model_path_adj}")
+
     return None, model_path_adj, all_losses[-1]
 
 
---
+++ code/training/main_old.py
@@ -0,0 +1,207 @@
+
+
+import code.dataset_code.utils as dataset_utils
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(filename)s:%(lineno)d - %(message)s"
+)
+
+from typing import Callable, Dict, List, Tuple
+import sys
+from datetime import datetime
+
+
+import code.training.check as training
+from code.dataset_code.dataset import Dataset_RL4CO
+
+
+# 1 adj ordered
+# 2 adj
+# 3 f ordered
+# 4 f
+
+# maps file execution parameter (1-4) to some model to train
+model_to_train = None
+if len(sys.argv) > 1:
+    model_to_train = int(sys.argv[1])
+    logging.info(f"Training models nr {model_to_train}")
+else:
+    logging.info("Please provide a training number!")
+
+
+def map_file_parameter_to_model_type(model_to_train: int) -> Tuple[str, bool]:
+    model_type = None
+    order = None
+    if model_to_train < 3:
+        model_type = "adj"
+    elif model_to_train > 2:
+        model_type = "f"
+    if model_to_train == 2 or model_to_train == 4:
+        order = False
+    elif model_to_train == 1 or model_to_train == 3:
+        order = True
+
+    logging.info(f"Using model ... type: {model_type}, order: {order}")
+    if model_type == None or order == None:
+        raise ValueError("That model type dosent exist. pecify one between 1 and 2")
+
+    return model_type, order
+
+model_type, order = map_file_parameter_to_model_type(model_to_train)
+
+
+def train_models(
+    model_type, order,
+    generator_params,
+    embed_size,
+    h, w,
+    testing_epochs, run_epochs,
+    graph_name, graph_save_folder,
+    train_dataset_path, test_dataset_path,
+    model_path_enc, model_path_adj,
+    valid_h, valid_w
+    ):
+
+
+    train_dataset = Dataset_RL4CO(train_dataset_path, generator_params, order, h, w)
+    test_dataset = Dataset_RL4CO(test_dataset_path, generator_params, order, h, w)
+
+    lrs = [1e-3, 1e-4, 1e-5, 1e-6]
+   
+    best_loss = 1
+    best_lr = None
+
+    logging.info(f"Began training model {model_type} order_{order} at time {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
+
+    for lr in lrs:
+        logging.info(f"Training with lr {lr}")
+        path_enc, path_adj, last_epoch_loss = training.diffusion(
+            model_type, train_dataset, test_dataset,
+            embed_size, model_path_adj, model_path_enc,
+            graph_name, graph_save_folder, valid_h, valid_w, testing_epochs, lr
+        ) 
+        if best_loss > last_epoch_loss: 
+            best_lr = lr
+            best_loss = last_epoch_loss
+
+    logging.info(f"Best lr found: {best_lr}, for model {model_type} order_{order} training full model now")
+    path_enc, path_adj, last_epoch_loss = training.diffusion(
+        model_type, train_dataset, test_dataset,
+        embed_size, model_path_adj, model_path_enc,
+        graph_name, graph_save_folder, valid_h, valid_w, run_epochs, best_lr
+    )
+
+    logging.info(f"Ended training model {model_type} order_{order} at time {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
+
+
+
+def four():
+    
+    generator_params = {
+        "num_jobs": 4,
+        "num_machines": 4,
+        "min_ops_per_job": 4,
+        "max_ops_per_job": 4,
+        "min_processing_time": 5,
+        "max_processing_time": 50,
+        "min_eligible_ma_per_op": 4,
+        "max_eligible_ma_per_op": 4,
+    }
+
+    embed_size = 80
+
+    h = 24
+    w = 24
+    valid_h = 4
+    valid_w = 16
+    testing_epochs = 2
+    run_epochs = 100
+
+
+    graph_name = f"model {model_type}, with order: {order}"
+
+    full_path = '/cluster/datastore/vemundvb/diffusion/diff_project/mindre_prosjekt'
+
+    graph_save_folder = f"{full_path}/results/444" 
+    train_dataset_path = f'{full_path}/data/batched_444'
+    test_dataset_path = f'{full_path}/data/batched_444_TEST'
+
+    
+    model_path_enc = f'{full_path}/models/444/enc_type_{model_type}_order_{order}.pth'
+    model_path_adj = f'{full_path}/models/444/adj_type_{model_type}_order_{order}.pth'
+    
+    return generator_params, embed_size,h, w, testing_epochs, run_epochs, graph_name, graph_save_folder, train_dataset_path, test_dataset_path, model_path_enc, model_path_adj, valid_h, valid_w
+    
+
+
+def mk01():
+    filepath_brandimarte_instance = '/cluster/datastore/vemundvb/diffusion/diff_project/mindre_prosjekt/brandimarte/mk01.txt'
+    parameters = dataset_utils.get_rl4co_parameters_from_brandimarte_instance(filepath_brandimarte_instance)
+    logging.info(parameters)
+
+    generator_params = {
+        "num_jobs": parameters['n_jobs'],
+        "num_machines": parameters['n_machines'],
+        "min_ops_per_job": parameters['fewest_operations'],
+        "max_ops_per_job": parameters['most_operations'],
+        "min_processing_time": parameters['min_processing_time'],
+        "max_processing_time": parameters['max_processing_time'],
+        "min_eligible_ma_per_op": parameters['min_machine_options'],
+        "max_eligible_ma_per_op": parameters['max_machine_options'],
+    }
+    """
+    generator_params = {
+        "num_jobs": 4,
+        "num_machines": 4,
+        "min_ops_per_job": 4,
+        "max_ops_per_job": 4,
+        "min_processing_time": 5,
+        "max_processing_time": 50,
+        "min_eligible_ma_per_op": 4,
+        "max_eligible_ma_per_op": 4,
+    }
+    """
+
+    embed_size = 80
+
+    h = 24
+    w = 64
+    valid_h = 6
+    valid_w = 60
+    testing_epochs = 1
+    run_epochs = 50
+
+
+    graph_name = f"model {model_type}, with order: {order}"
+
+    full_path = '/cluster/datastore/vemundvb/diffusion/diff_project/mindre_prosjekt'
+
+    graph_save_folder = f"{full_path}/results/mk01" 
+    train_dataset_path = f'{full_path}/data/batched_mk01_10j_6ma_6op_mk01'
+    test_dataset_path = f'{full_path}/data/batched_mk01_10j_6ma_6op_mk01_TEST'
+
+    
+    model_path_enc = f'{full_path}/models/mk01/enc_type_{model_type}_order_{order}.pth'
+    model_path_adj = f'{full_path}/models/mk01/oldy/adj_type_{model_type}_order_{order}.pth'
+    
+    return generator_params, embed_size,h, w, testing_epochs, run_epochs, graph_name, graph_save_folder, train_dataset_path, test_dataset_path, model_path_enc, model_path_adj, valid_h, valid_w
+    
+
+
+# generator_params, embed_size,h, w, testing_epochs, run_epochs, graph_name, graph_save_folder, train_dataset_path, test_dataset_path, model_path_enc, model_path_adj, valid_h, valid_w = four()
+generator_params, embed_size,h, w, testing_epochs, run_epochs, graph_name, graph_save_folder, train_dataset_path, test_dataset_path, model_path_enc, model_path_adj, valid_h, valid_w = mk01()
+
+train_models(model_type, order,
+    generator_params,
+    embed_size,
+    h, w,
+    testing_epochs, run_epochs,
+    graph_name, graph_save_folder,
+    train_dataset_path, test_dataset_path,
+    model_path_enc, model_path_adj, 
+    valid_h, valid_w)
+ 
+
+
